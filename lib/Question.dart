0778a44456126f42a77f59ed249c813594a83d0d
bloodkalsura <64530853+bloodkalsura@users.noreply.github.com>
2021-10-11 22:40:05 +0530

    Update Question.dart
    Updated for a better year capture

--- lib/Question.dart
+++ lib/Question.dart
@@ -11,6 +11,22 @@ class Question{
     else
       print("You've chosen False as your answer");
       
-      
+          if (year >= 2001) {
+        print('21st century');
+      } else if (year >= 1901) {
+        print('20th century');
+      }
+
+      for (var object in flybyObjects) {
+        print(object);
+      }
+
+      for (int month = 1; month <= 12; month++) {
+        print(month);
+      }
+
+      while (year < 2016) {
+        year += 1;
+      }   
   }
 }
